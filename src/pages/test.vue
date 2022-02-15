fbdd3cb67539c1444a9965aa0d407b55b01a9a01
farahinani <farahinaniahmad@gmail.com>
2022-02-15 12:44:59 +0800

    test button

--- src/pages/test.vue
+++ src/pages/test.vue
@@ -3,54 +3,54 @@
     <q-btn
       id="accelPermsButton"
       color="primary"
-      label="btn onclick()"
-      @click="onClick"
-    />
-    <q-btn
-      id="accelPermsButton"
-      color="primary"
-      label="btn getAccel()"
-      @click="getAccel"
+      label="test btn"
+      @click="myButton"
     />
   </div>
 </template>
 
 <script>
-import { defineComponent } from "vue";
+import { defineComponent, ref } from "vue";
+import { PluginListenerHandle } from "@capacitor/core";
+import { Motion } from "@capacitor/motion";
+
+// let accelHandler: PluginListenerHandle;
 
 export default defineComponent({
   name: "test",
 
   methods() {
-    function onClick() {
-      // feature detect
-      if (typeof DeviceMotionEvent.requestPermission === "function") {
-        DeviceMotionEvent.requestPermission()
-          .then((permissionState) => {
-            if (permissionState === "granted") {
-              window.addEventListener("devicemotion", () => {});
-            }
-          })
-          .catch(console.error);
-      } else {
-        // handle regular non iOS 13+ devices
-        console.log("test");
+    async function mybutton() {
+      try {
+        await DeviceMotionEvent.requestPermission();
+      } catch (e) {
+        // Handle error
+        return;
       }
-    }
 
-    function getAccel() {
-      DeviceMotionEvent.requestPermission().then((response) => {
-        if (response == "granted") {
-          console.log("accelerometer permission granted");
-          // Do stuff here
-        }
+      // Once the user approves, can start listening:
+      accelHandler = await Motion.addListener("accel", (event) => {
+        console.log("Device motion event:", event);
       });
-    }
 
-    return {
-      getAccel,
-      onClick,
-    };
+      // Stop the acceleration listener
+      const stopAcceleration = () => {
+        if (accelHandler) {
+          accelHandler.remove();
+        }
+      };
+
+      // Remove all listeners
+      const removeListeners = () => {
+        Motion.removeAllListeners();
+      };
+
+      return {
+        stopAcceleration,
+        removeListeners,
+        mybutton,
+      };
+    }
   },
 });
 </script>
