f63fabc07681800d85f5252bfee51ef82a8f9e2d
farahinani <farahinaniahmad@gmail.com>
2022-02-15 18:51:29 +0800

    add another button to test

--- src/pages/test.vue
+++ src/pages/test.vue
@@ -4,20 +4,23 @@
       id="accelPermsButton"
       color="primary"
       @click="getAccel()"
-      label="test btn 2"
+      label="test btn"
     >
     </q-btn>
+    <q-btn color="primary" @click="myButton" label="test btn 2"> </q-btn>
   </div>
 </template>
 
 <script>
 import { defineComponent } from "vue";
+import { PluginListenerHandle } from "@capacitor/core";
+import { Motion } from "@capacitor/motion";
 
 export default defineComponent({
   name: "test",
 
-  methods: {
-    getAccel() {
+  setup() {
+    function getAccel() {
       DeviceMotionEvent.requestPermission().then((response) => {
         if (response == "granted") {
           // Add a listener to get smartphone acceleration
@@ -32,7 +35,39 @@ export default defineComponent({
           });
         }
       });
-    },
+    }
+
+    myButton.addEventListener("click", async () => {
+      try {
+        await DeviceMotionEvent.requestPermission();
+      } catch (e) {
+        // Handle error
+        return;
+      }
+
+      // Once the user approves, can start listening:
+      accelHandler = await Motion.addListener("accel", (event) => {
+        console.log("Device motion event:", event);
+      });
+    });
+
+    // Stop the acceleration listener
+    const stopAcceleration = () => {
+      if (accelHandler) {
+        accelHandler.remove();
+      }
+    };
+
+    // Remove all listeners
+    const removeListeners = () => {
+      Motion.removeAllListeners();
+    };
+
+    return {
+      getAccel,
+      stopAcceleration,
+      removeListeners,
+    };
   },
 });
 </script>
