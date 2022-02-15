8b5c18c45321426d24abd697ca86340ddb8c3800
farahinani <farahinaniahmad@gmail.com>
2022-02-15 10:19:14 +0800

    add getAccel()

--- src/pages/test.vue
+++ src/pages/test.vue
@@ -1,47 +1,35 @@
 <template>
   <div>
-    <q-btn color="primary" label="Request permission" @click="myButton" />
+    <q-btn color="primary" label="Request permission" @click="getAccel()" />
   </div>
 </template>
 
 <script>
 import { defineComponent } from "vue";
-import { PluginListenerHandle } from "@capacitor/core";
-import { Motion } from "@capacitor/motion";
 
 export default defineComponent({
   name: "test",
 
   methods() {
-    myButton.addEventListener("click", async () => {
-      try {
-        await DeviceMotionEvent.requestPermission();
-      } catch (e) {
-        // Handle error
-        return;
-      }
-
-      // Once the user approves, can start listening:
-      accelHandler = await Motion.addListener("accel", (event) => {
-        console.log("Device motion event:", event);
+    function getAccel() {
+      DeviceMotionEvent.requestPermission().then((response) => {
+        if (response == "granted") {
+          // Add a listener to get smartphone acceleration
+          // in the XYZ axes (units in m/s^2)
+          window.addEventListener("devicemotion", (event) => {
+            console.log(event);
+          });
+          // Add a listener to get smartphone orientation
+          // in the alpha-beta-gamma axes (units in degrees)
+          window.addEventListener("deviceorientation", (event) => {
+            console.log(event);
+          });
+        }
       });
-    });
-
-    // Stop the acceleration listener
-    const stopAcceleration = () => {
-      if (accelHandler) {
-        accelHandler.remove();
-      }
-    };
-
-    // Remove all listeners
-    const removeListeners = () => {
-      Motion.removeAllListeners();
-    };
+    }
 
     return {
-      stopAcceleration,
-      removeListeners,
+      getAccel,
     };
   },
 });
