4057a741e66c6e38e29337cd726bd5ee2f6d3247
farahinani <farahinaniahmad@gmail.com>
2022-02-15 13:25:51 +0800

    test button

--- src/pages/test.vue
+++ src/pages/test.vue
@@ -27,22 +27,16 @@ export default defineComponent({
       DeviceMotionEvent.requestPermission().then((response) => {
         if (response == "granted") {
           // Add a listener to get smartphone acceleration
-          // in the XYZ axes (units in m/s^2)
           window.addEventListener("devicemotion", (event) => {
             console.log(event);
           });
-          // Add a listener to get smartphone orientation
-          // in the alpha-beta-gamma axes (units in degrees)
+          // Add a listener to get smartphone orientatio
           window.addEventListener("deviceorientation", (event) => {
             console.log(event);
           });
         }
       });
     }
-
-    return {
-      getAccel,
-    };
   },
 });
 </script>
