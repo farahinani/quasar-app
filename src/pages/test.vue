0f40f36e99ce1e3cd8b0180d68b5bae5ef1a25ff
farahinani <farahinaniahmad@gmail.com>
2022-02-15 22:45:18 +0800

    add shake function alert

--- src/pages/test.vue
+++ src/pages/test.vue
@@ -12,23 +12,56 @@
 
 <script>
 import { defineComponent } from "vue";
+import { PluginListenerHandle } from "@capacitor/core";
+import { Motion } from "@capacitor/motion";
+
+// let accelHandler: PluginListenerHandle;
 
 export default defineComponent({
   name: "test",
 
   methods: {
     getAccel() {
+      var oldx = 0;
+      var oldy = 0;
+
+      var shakethreshold = 10;
+
+      // if (window.DeviceMotionEvent) {
+      //   window.addEventListener("devicemotion", function () {
+      //     if (
+      //       Math.abs(oldx - Math.round(event.acceleration.x)) >
+      //         shakethreshold ||
+      //       Math.abs(oldy - Math.round(event.acceleration.y)) > shakethreshold
+      //     ) {
+
+      //       alert("shaken");
+      //     }
+      //     oldx = Math.round(accel.x);
+      //     oldy = Math.round(accel.y);
+      //   });
+      // }
+
       DeviceMotionEvent.requestPermission().then((response) => {
         if (response == "granted") {
-          // Add a listener to get smartphone acceleration
           window.addEventListener("devicemotion", (event) => {
             console.log(event);
+
+            if (
+              Math.abs(oldx - Math.round(event.acceleration.x)) >
+                shakethreshold ||
+              Math.abs(oldy - Math.round(event.acceleration.y)) > shakethreshold
+            ) {
+              //shaken, do something
+              alert("shaken");
+            }
+            oldx = Math.round(accel.x);
+            oldy = Math.round(accel.y);
           });
 
-          // Add a listener to get smartphone orientation
-          // window.addEventListener('deviceorientation',(event) => {
-          //     console.log(event);
-          // });
+          window.addEventListener("deviceorientation", (event) => {
+            console.log(event);
+          });
         }
       });
     },
