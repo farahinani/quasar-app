f38e7bb82643fc6296c24a0d0ade98f5a37164ce
farahinani <farahinaniahmad@gmail.com>
2022-02-15 21:22:32 +0800

    add shake function

--- src/pages/test.vue
+++ src/pages/test.vue
@@ -25,53 +25,38 @@ import { Motion } from "@capacitor/motion";
 export default defineComponent({
   name: "test",
 
-  methods() {
-    function getAccel() {
+  methods: {
+    getAccel() {
       DeviceMotionEvent.requestPermission().then((response) => {
         if (response == "granted") {
           // Add a listener to get smartphone acceleration
-          // in the XYZ axes (units in m/s^2)
-          window.addEventListener("devicemotion", (event) => {
-            console.log(event);
+          window.addEventListener("devicemotion", () => {
+            // console.log(event);
+
+            var myShakeEvent = new Shake({
+              threshold: 15,
+            });
+
+            // start listening to device motion
+            myShakeEvent.start();
+
+            // register a shake event
+            window.addEventListener("shake", shakeEventDidOccur, false);
           });
+
           // Add a listener to get smartphone orientation
-          // in the alpha-beta-gamma axes (units in degrees)
           window.addEventListener("deviceorientation", (event) => {
             console.log(event);
           });
+
+          //shake event callback
+          function shakeEventDidOccur() {
+            console.log("shake");
+            alert("Shake!");
+          }
         }
       });
-    }
-
-    async function myButton() {
-      try {
-        await DeviceMotionEvent.requestPermission();
-      } catch (e) {
-        return;
-      }
-
-      // Once the user approves, can start listening:
-      accelHandler = await Motion.addListener("accel", (event) => {
-        console.log("Device motion event:", event);
-      });
-
-      // Stop the acceleration listener
-      const stopAcceleration = () => {
-        if (accelHandler) {
-          accelHandler.remove();
-        }
-      };
-
-      // Remove all listeners
-      const removeListeners = () => {
-        Motion.removeAllListeners();
-      };
-    }
-
-    return {
-      myButton,
-      getAccel,
-    };
+    },
   },
 });
 </script>
