b4d0b624e08059917e01cdcb12f99371877425fd
farahinani <farahinaniahmad@gmail.com>
2022-02-15 13:19:00 +0800

    test button

--- src/pages/test.vue
+++ src/pages/test.vue
@@ -4,53 +4,45 @@
       id="accelPermsButton"
       color="primary"
       label="test btn"
-      @click="myButton"
+      @click="getAccel"
     />
+    <q-btn
+      id="accelPermsButton"
+      color="primary"
+      onclick="getAccel()"
+      label="test btn 2"
+    >
+    </q-btn>
   </div>
 </template>
 
 <script>
-import { defineComponent, ref } from "vue";
-import { PluginListenerHandle } from "@capacitor/core";
-import { Motion } from "@capacitor/motion";
-
-// let accelHandler: PluginListenerHandle;
+import { defineComponent } from "vue";
 
 export default defineComponent({
   name: "test",
 
-  methods() {
-    async function mybutton() {
-      try {
-        await DeviceMotionEvent.requestPermission();
-      } catch (e) {
-        // Handle error
-        return;
-      }
-
-      // Once the user approves, can start listening:
-      accelHandler = await Motion.addListener("accel", (event) => {
-        console.log("Device motion event:", event);
-      });
-
-      // Stop the acceleration listener
-      const stopAcceleration = () => {
-        if (accelHandler) {
-          accelHandler.remove();
+  setup() {
+    function getAccel() {
+      DeviceMotionEvent.requestPermission().then((response) => {
+        if (response == "granted") {
+          // Add a listener to get smartphone acceleration
+          // in the XYZ axes (units in m/s^2)
+          window.addEventListener("devicemotion", (event) => {
+            console.log(event);
+          });
+          // Add a listener to get smartphone orientation
+          // in the alpha-beta-gamma axes (units in degrees)
+          window.addEventListener("deviceorientation", (event) => {
+            console.log(event);
+          });
         }
-      };
-
-      // Remove all listeners
-      const removeListeners = () => {
-        Motion.removeAllListeners();
-      };
-
-      return {
-        stopAcceleration,
-        removeListeners,
-        mybutton,
-      };
+      });
     }
+
+    return {
+      getAccel,
+    };
   },
 });
 </script>
