b8bf20cc0853bc13ae42413eed6aca105be75be2
farahinani <farahinaniahmad@gmail.com>
2022-02-15 08:18:09 +0800

    update permission @ test page

--- src/pages/test.vue
+++ src/pages/test.vue
@@ -1,9 +1,10 @@
 <template>
   <div>
     <q-btn
+      id="accelPermsButton"
       color="primary"
       label="Request permission"
-      @click="requestDeviceMotion"
+      @click="getAccel()"
     />
   </div>
 </template>
@@ -17,27 +18,25 @@ export default defineComponent({
   name: "test",
 
   setup() {
-    function requestDeviceMotion(callback) {
-      if (window.DeviceMotionEvent == null) {
-        callback(new Error("DeviceMotion is not supported."));
-      } else if (DeviceMotionEvent.requestPermission) {
-        DeviceMotionEvent.requestPermission().then(
-          function (state) {
-            if (state == "granted") {
-              callback(null);
-            } else callback(new Error("Permission denied by user"));
-          },
-          function (err) {
-            callback(err);
-          }
-        );
-      } else {
-        // no need for permission
-        callback(null);
-      }
+    function getAccel() {
+      DeviceMotionEvent.requestPermission().then((response) => {
+        if (response == "granted") {
+          // Add a listener to get smartphone acceleration
+          // in the XYZ axes (units in m/s^2)
+          window.addEventListener("devicemotion", (event) => {
+            console.log(event);
+          });
+          // Add a listener to get smartphone orientation
+          // in the alpha-beta-gamma axes (units in degrees)
+          window.addEventListener("deviceorientation", (event) => {
+            console.log(event);
+          });
+        }
+      });
     }
+
     return {
-      requestDeviceMotion,
+      getAccel,
     };
   },
 });
