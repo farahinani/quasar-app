b32e2d3cbe89b918eb8ef4a9b262c092b9dadccd
farahinani <farahinaniahmad@gmail.com>
2022-02-15 08:23:43 +0800

    update permission @ test page

--- src/pages/test.vue
+++ src/pages/test.vue
@@ -1,18 +1,11 @@
 <template>
   <div>
-    <q-btn
-      id="accelPermsButton"
-      color="primary"
-      label="Request permission"
-      @click="getAccel()"
-    />
+    <q-btn color="primary" label="Request permission" @click="getAccel()" />
   </div>
 </template>
 
 <script>
 import { defineComponent } from "vue";
-import { ref } from "vue";
-import { Camera, CameraResultType } from "@capacitor/camera";
 
 export default defineComponent({
   name: "test",
@@ -21,16 +14,8 @@ export default defineComponent({
     function getAccel() {
       DeviceMotionEvent.requestPermission().then((response) => {
         if (response == "granted") {
-          // Add a listener to get smartphone acceleration
-          // in the XYZ axes (units in m/s^2)
-          window.addEventListener("devicemotion", (event) => {
-            console.log(event);
-          });
-          // Add a listener to get smartphone orientation
-          // in the alpha-beta-gamma axes (units in degrees)
-          window.addEventListener("deviceorientation", (event) => {
-            console.log(event);
-          });
+          console.log("accelerometer permission granted");
+          // Do stuff here
         }
       });
     }
