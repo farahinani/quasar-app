d301272c6b837c7d3cbb525d89b00a5a731f8915
farahinani <farahinaniahmad@gmail.com>
2022-02-16 10:55:58 +0800

    update platform detection

--- src/pages/test.vue
+++ src/pages/test.vue
@@ -1,30 +1,8 @@
 <template>
   <div class="q-pa-md">
-    <div class="q-mb-md">
-      Browser User Agent: "<strong>{{ $q.platform.userAgent }}</strong
-      >"
-    </div>
-
-    <q-markup-table flat bordered>
-      <thead>
-        <tr>
-          <th class="text-left">Property</th>
-          <th class="text-left">Value</th>
-        </tr>
-      </thead>
-
-      <tbody>
-        <tr v-for="(value, prop) in $q.platform.is" :key="prop">
-          <td>{{ prop }}</td>
-          <td>{{ value }}</td>
-        </tr>
-      </tbody>
-    </q-markup-table>
-
-    <div class="q-mt-md">
-      The device which you are using to view this website
-      <strong>{{ touch }}</strong> touch capability.
-    </div>
+    <div v-if="$q.platform.is.desktop">I'm only rendered on Desktop!</div>
+    <div v-if="$q.platform.is.android">I'm only rendered on mobile!</div>
+    <div v-if="$q.platform.is.ios">I'm only rendered on mobile!</div>
   </div>
 </template>
 
@@ -39,40 +17,10 @@ export default defineComponent({
   setup() {
     const $q = useQuasar();
 
-    return {
-      touch: computed(() => ($q.platform.has.touch ? "has" : "does not have")),
-    };
+    $q.platform.is.desktop;
+    $q.platform.is.android;
+    $q.platform.is.ios;
   },
-
-  // methods: {
-  //   getAccel() {
-  //     var oldx = 0;
-  //     var oldy = 0;
-
-  //     var shakethreshold = 30;
-
-  //     DeviceMotionEvent.requestPermission().then((response) => {
-  //       if (response == "granted") {
-  //         window.addEventListener("devicemotion", (event) => {
-
-  //           if (
-  //             Math.abs(oldx - Math.round(event.acceleration.x)) >
-  //               shakethreshold ||
-  //             Math.abs(oldy - Math.round(event.acceleration.y)) > shakethreshold
-  //           ) {
-  //             alert("shaken");
-  //           }
-  //           oldx = Math.round(accel.x);
-  //           oldy = Math.round(accel.y);
-  //         });
-
-  //         window.addEventListener("deviceorientation", (event) => {
-  //           console.log(event);
-  //         });
-  //       }
-  //     });
-  //   },
-  // },
 });
 </script>
 
