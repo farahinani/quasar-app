229914ea00193aaf8eb9d898b178977d2eb38ef2
farahinani <farahinaniahmad@gmail.com>
2022-02-17 18:23:37 +0800

    add tries and count

--- src/pages/PageShakeWin.vue
+++ src/pages/PageShakeWin.vue
@@ -5,8 +5,10 @@
         <q-card-section>
           <div class="q-py-lg q-px-md">
             <div class="text-h5 text-center">
-              You have 2 tries
-              <q-badge color="teal-10" align="middle"> 1/2 </q-badge>
+              You have : {{ $root.numTries }} tries
+              <q-badge color="teal-10" align="middle">
+                {{ $root.triesCount }}/{{ $root.numTries }}
+              </q-badge>
             </div>
           </div>
 
@@ -22,6 +24,14 @@
             >
             </q-btn>
           </div>
+          <br />
+          <q-btn
+            type="submit"
+            label="SHAKE BUTTON"
+            color="primary"
+            class="full-width"
+            @click="shakeSuccess()"
+          />
         </q-card-section>
       </q-card>
     </div>
@@ -106,74 +116,19 @@ export default defineComponent({
     }
   },
 
-  // methods: {
-  //   getAccel() {
-  //     var oldx = 0;
-  //     var oldy = 0;
+  methods: {
+    shakeSuccess() {
+      //JOANNE's code
+      if (this.$root.triesCount < this.$root.numTries) {
+        this.$root.triesCount++;
 
-  //     var shakethreshold = 20;
-
-  //     DeviceMotionEvent.requestPermission().then((response) => {
-  //       if (response == "granted") {
-  //         window.addEventListener("devicemotion", (event) => {
-  //           // console.log(event);
-
-  //           if (
-  //             Math.abs(oldx - Math.round(event.acceleration.x)) >
-  //               shakethreshold ||
-  //             Math.abs(oldy - Math.round(event.acceleration.y)) > shakethreshold
-  //           ) {
-  //             //shaken, do something
-  //             alert("shaken !");
-  //           }
-  //           oldx = Math.round(accel.x);
-  //           oldy = Math.round(accel.y);
-  //         });
-
-  //         window.addEventListener("deviceorientation", (event) => {
-  //           console.log(event);
-  //         });
-  //       }
-  //     });
-  //   },
-  // },
-
-  //DONT DELETE THIS. WORK IN PROGRESS//
-  // setup() {
-  //   var px = 50;
-  //   var py = 50;
-  //   var vx = 0.0;
-  //   var vy = 0.0;
-  //   var updateRate = 1 / 60;
-  //   function getAccel() {
-  //     DeviceMotionEvent.requestPermission().then((response) => {
-  //       if (response == "granted") {
-  //         window.addEventListener("deviceorientation", (event) => {
-  //           rotation_degrees = event.alpha;
-  //           frontToBack_degrees = event.beta;
-  //           leftToRight_degrees = event.gamma;
-
-  //           vx = vx + leftToRight_degrees * updateRate * 2;
-  //           vy = vy + frontToBack_degrees * updateRate;
-
-  //           px = px + vx * 0.5;
-  //           if (px > 98 || px < 0) {
-  //             px = Math.max(0, Math.min(98, px));
-  //             vx = 0;
-  //           }
-
-  //           py = py + vy * 0.5;
-  //           if (py > 98 || py < 0) {
-  //             py = Math.max(0, Math.min(98, py));
-  //             vy = 0;
-  //           }
-
-  //           dot = document.getElementsByClassName("indicatorDot")[0];
-  //           dot.setAttribute("style", "left:" + px + "%;" + "top:" + py + "%;");
-  //         });
-  //       }
-  //     });
-  //   }
-  // },
+        if (this.$root.triesCount < this.$root.numTries) {
+          alert("shaken !! : try " + this.$root.triesCount);
+        } else {
+          alert("Last Shake");
+        }
+      }
+    },
+  },
 });
 </script>
