96495171cac12dfd684855c79a19018b99658bac
farahinani <farahinaniahmad@gmail.com>
2022-02-18 10:01:44 +0800

    Update android shake

--- src/pages/PageShakeWin.vue
+++ src/pages/PageShakeWin.vue
@@ -160,26 +160,46 @@ export default defineComponent({
       var oldy = 0;
       var shakethreshold = 20;
 
-      window.addEventListener("devicemotion", (event) => {
-        if (
-          Math.abs(oldx - Math.round(event.acceleration.x)) > shakethreshold ||
-          Math.abs(oldy - Math.round(event.acceleration.y)) > shakethreshold
-        ) {
-          // alert("shaken !");
-          if (this.$root.triesCount < this.$root.numTries) {
-            alert("shaken !! : try " + this.$root.triesCount);
-            this.$root.triesCount += 1;
-          } else {
-            // alert("Last Shake");
-            alert("finish shake"); // go to home
+      if (this.$root.triesCount < this.$root.numTries) {
+        window.addEventListener("devicemotion", (event) => {
+          if (
+            Math.abs(oldx - Math.round(event.acceleration.x)) >
+              shakethreshold ||
+            Math.abs(oldy - Math.round(event.acceleration.y)) > shakethreshold
+          ) {
+            if (this.$root.triesCount < this.$root.numTries) {
+              alert("shaken !! : try " + this.$root.triesCount);
+              this.$root.triesCount += 1;
+            } else {
+              alert("finish shake");
+            }
           }
-        }
-        oldx = Math.round(accel.x);
-        oldy = Math.round(accel.y);
-      });
-    } else {
-      alert("this is dekstop!");
+          oldx = Math.round(accel.x);
+          oldy = Math.round(accel.y);
+        });
+      } else {
+        alert("this is dekstop!");
+      }
     }
+
+    //   window.addEventListener("devicemotion", (event) => {
+    //     if (
+    //       Math.abs(oldx - Math.round(event.acceleration.x)) > shakethreshold ||
+    //       Math.abs(oldy - Math.round(event.acceleration.y)) > shakethreshold
+    //     ) {
+    //       if (this.$root.triesCount < this.$root.numTries) {
+    //         alert("shaken !! : try " + this.$root.triesCount);
+    //         this.$root.triesCount += 1;
+    //       } else {
+    //         alert("finish shake");
+    //       }
+    //     }
+    //     oldx = Math.round(accel.x);
+    //     oldy = Math.round(accel.y);
+    //   });
+    // } else {
+    //   alert("this is dekstop!");
+    // }
   },
 
   methods: {
