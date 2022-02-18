dd047b0a790b82e1ea1c2ffef3b192512554e34e
farahinani <farahinaniahmad@gmail.com>
2022-02-18 16:08:00 +0800

    add alexgibson code 3

--- src/pages/PageShakeWin.vue
+++ src/pages/PageShakeWin.vue
@@ -171,18 +171,23 @@ export default defineComponent({
       var shakethreshold = 20;
 
       //-------------------2nd CODE HERE. CURRENTLY FARAH IS WORKING ON THIS [alexgibson]---------------------------------//
+
       window.onload = function () {
+        //create a new instance of shake.js.
         var myShakeEvent = new Shake({
-          threshold: 20,
+          threshold: 15,
         });
 
-        //start listening to device motion
+        // start listening to device motion
         myShakeEvent.start();
 
+        // register a shake event
         window.addEventListener("shake", shakeEventDidOccur, false);
 
+        //shake event callback
         function shakeEventDidOccur() {
-          alert("shaken");
+          //put your own code here etc.
+          alert("Shake!");
         }
       };
 
