49dadf35abfb90d6e06438a2a78ec4624bebf70b
farahinani <farahinaniahmad@gmail.com>
2022-02-18 12:47:16 +0800

    android shake update 14

--- src/pages/PageShakeWin.vue
+++ src/pages/PageShakeWin.vue
@@ -160,24 +160,22 @@ export default defineComponent({
       var oldy = 0;
       var shakethreshold = 20;
 
-      window.addEventListener("devicemotion", (event) => {
-        if (
-          Math.abs(oldx - Math.round(event.acceleration.x)) > shakethreshold ||
-          Math.abs(oldy - Math.round(event.acceleration.y)) > shakethreshold
-        ) {
-          if (this.$root.triesCount < this.$root.numTries) {
-            alert("shaken !! : try ");
-
+      if (this.$root.triesCount < this.$root.numTries) {
+        window.addEventListener("devicemotion", (event) => {
+          if (
+            Math.abs(oldx - Math.round(event.acceleration.x)) >
+              shakethreshold ||
+            Math.abs(oldy - Math.round(event.acceleration.y)) > shakethreshold
+          ) {
+            alert("shaken !! : try " + this.$root.triesCount);
             this.$root.triesCount += 1;
           } else {
-            alert("finish shake");
+            alert("not shaken");
           }
-        } else {
-          alert("not shaken");
-        }
-        oldx = Math.round(accel.x);
-        oldy = Math.round(accel.y);
-      });
+          oldx = Math.round(accel.x);
+          oldy = Math.round(accel.y);
+        });
+      }
 
       //-------------------2nd CODE HERE---------------------------------//
       // if (this.$root.triesCount < this.$root.numTries) {
