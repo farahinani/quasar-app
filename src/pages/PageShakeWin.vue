eed3635282115488aa7096321b2b1c42a2656d77
farahinani <farahinaniahmad@gmail.com>
2022-02-18 10:08:54 +0800

    Update android shake 2

--- src/pages/PageShakeWin.vue
+++ src/pages/PageShakeWin.vue
@@ -160,8 +160,10 @@ export default defineComponent({
       var oldy = 0;
       var shakethreshold = 20;
 
-      if (this.$root.triesCount < this.$root.numTries) {
-        window.addEventListener("devicemotion", (event) => {
+      //2nd code
+
+      window.addEventListener("devicemotion", (event) => {
+        if (this.$root.triesCount < this.$root.numTries) {
           if (
             Math.abs(oldx - Math.round(event.acceleration.x)) >
               shakethreshold ||
@@ -176,10 +178,31 @@ export default defineComponent({
           }
           oldx = Math.round(accel.x);
           oldy = Math.round(accel.y);
-        });
-      } else {
-        alert("this is dekstop!");
-      }
+        }
+      });
+
+      //1st code
+
+      // if (this.$root.triesCount < this.$root.numTries) {
+      //   window.addEventListener("devicemotion", (event) => {
+      //     if (
+      //       Math.abs(oldx - Math.round(event.acceleration.x)) >
+      //         shakethreshold ||
+      //       Math.abs(oldy - Math.round(event.acceleration.y)) > shakethreshold
+      //     ) {
+      //       if (this.$root.triesCount < this.$root.numTries) {
+      //         alert("shaken !! : try " + this.$root.triesCount);
+      //         this.$root.triesCount += 1;
+      //       } else {
+      //         alert("finish shake");
+      //       }
+      //     }
+      //     oldx = Math.round(accel.x);
+      //     oldy = Math.round(accel.y);
+      //   });
+      // }
+    } else {
+      alert("this is dekstop!");
     }
 
     //   window.addEventListener("devicemotion", (event) => {
