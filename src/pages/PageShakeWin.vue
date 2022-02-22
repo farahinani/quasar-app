6fcb66192673192f949248711936a7c6837c9f3e
farahinani <farahinaniahmad@gmail.com>
2022-02-22 16:40:13 +0800

    update android 5

--- src/pages/PageShakeWin.vue
+++ src/pages/PageShakeWin.vue
@@ -195,23 +195,29 @@ export default defineComponent({
       // alert("This is Android!");
 
       //-------------------NEW CODE HERE. CURRENTLY FARAH IS WORKING ON THIS ---------------------------------//
-      window.onload = function () {
-        //create a new instance of shake.js.
-        var myShakeEvent = new Shake({
-          threshold: 15,
-        });
+      function shaketest() {
+        window.onload = function () {
+          //create a new instance of shake.js.
+          var myShakeEvent = new Shake({
+            threshold: 15,
+          });
 
-        // start listening to device motion
-        myShakeEvent.start();
+          // start listening to device motion
+          myShakeEvent.start();
 
-        // register a shake event
-        window.addEventListener("shake", shakeEventDidOccur, false);
+          // register a shake event
+          window.addEventListener("shake", shakeEventDidOccur, false);
 
-        //shake event callback
-        function shakeEventDidOccur() {
-          //put your own code here etc.
-          alert("Shake!");
-        }
+          //shake event callback
+          function shakeEventDidOccur() {
+            //put your own code here etc.
+            alert("Shake!");
+          }
+        };
+      }
+
+      return {
+        shaketest,
       };
 
       //-------------------ORIGINAL CODE HERE---------------------------------//
