bd088bd573aed34c0d37f222e81963ccc46843b9
farahinani <farahinaniahmad@gmail.com>
2022-02-23 17:06:53 +0800

    text alexgibson 6

--- src/pages/test.vue
+++ src/pages/test.vue
@@ -19,7 +19,7 @@
 
 <script>
 import { defineComponent } from "vue";
-import { Shake } from "src/router/shake.js";
+import { Shake } from "src/router/shake";
 
 export default defineComponent({
   name: "test",
