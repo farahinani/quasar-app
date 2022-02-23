e56921c027897e8d530805acf18e1d4dae73b49c
farahinani <farahinaniahmad@gmail.com>
2022-02-23 16:52:54 +0800

    text alexgibson 4

--- src/pages/test.vue
+++ src/pages/test.vue
@@ -17,27 +17,34 @@ import { Shake } from "src/router/shake.js";
 export default defineComponent({
   name: "test",
 
-  setup() {},
+  setup() {
+    var myShakeEvent = new Shake({ threshold: 15, timeout: 1000 });
+    myShakeEvent.start();
+    window.addEventListener(
+      "shake",
+      function () {
+        alert("shake!!!!");
+      },
+      false
+    );
+  },
 
   method() {
-    window.onload = function () {
-      //create a new instance of shake.js.
-      var myShakeEvent = new Shake({
-        threshold: 10,
-      });
-
-      // start listening to device motion
-      myShakeEvent.start();
-
-      // register a shake event
-      window.addEventListener("shake", shakeEventDidOccur, false);
-
-      //shake event callback
-      function shakeEventDidOccur() {
-        //put your own code here etc.
-        alert("Shake!");
-      }
-    };
+    // window.onload = function () {
+    //   //create a new instance of shake.js.
+    //   var myShakeEvent = new Shake({
+    //     threshold: 10,
+    //   });
+    //   // start listening to device motion
+    //   myShakeEvent.start();
+    //   // register a shake event
+    //   window.addEventListener("shake", shakeEventDidOccur, false);
+    //   //shake event callback
+    //   function shakeEventDidOccur() {
+    //     //put your own code here etc.
+    //     alert("Shake!");
+    //   }
+    // };
   },
 });
 </script>
