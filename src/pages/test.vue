cc1379827b2850c254e772f6c4cf33e7403282c1
farahinani <farahinaniahmad@gmail.com>
2022-02-23 15:39:48 +0800

    update page test 4

--- src/pages/test.vue
+++ src/pages/test.vue
@@ -20,23 +20,36 @@ export default defineComponent({
   setup() {
     var oldx = 0;
     var oldy = 0;
-    var shakethreshold = 10;
 
-    if (window.DeviceMotionEvent) {
-      window.addEventListener(
-        "devicemotion",
-        function () {
-          if (
-            Math.abs(oldx - Math.round(acceleration.x)) > shakethreshold ||
-            Math.abs(oldy - Math.round(acceleration.y)) > shakethreshold
-          ) {
-            alert("shaken");
-          }
-          oldx = Math.round(accel.x);
-          oldy = Math.round(accel.y);
-        },
-        true
-      );
+    var shakethreshold = 25;
+
+    if (this.$root.triesCount < this.$root.numTries) {
+      DeviceMotionEvent.requestPermission().then((response) => {
+        if (response == "granted") {
+          window.addEventListener(
+            "devicemotion",
+            (event) => {
+              if (
+                Math.abs(oldx - Math.round(event.acceleration.x)) >
+                  shakethreshold ||
+                Math.abs(oldy - Math.round(event.acceleration.y)) >
+                  shakethreshold
+              ) {
+                alert("shaken!");
+                // if (this.$root.triesCount < this.$root.numTries) {
+                //   alert("shaken !! : try " + this.$root.triesCount);
+                //   this.$root.triesCount += 1;
+                // } else {
+                //   alert("finish shake");
+                // }
+              }
+              oldx = Math.round(accel.x);
+              oldy = Math.round(accel.y);
+            },
+            true
+          );
+        }
+      });
     }
   },
 
