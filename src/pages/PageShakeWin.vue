964b23850c932a51bb798c80bb27ff97b14651ea
farahinani <farahinaniahmad@gmail.com>
2022-02-24 18:18:18 +0800

    update android shake 8

--- src/pages/PageShakeWin.vue
+++ src/pages/PageShakeWin.vue
@@ -261,11 +261,7 @@ export default defineComponent({
 
       //shake event callback
       function shakeEventDidOccur() {
-        if (this.$root.triesCount < this.$root.numTries) {
-          alert("shaken !! : try " + this.$root.triesCount);
-        } else {
-          alert("Last Shake !!");
-        }
+        alert("shaken !! : try " + this.$root.triesCount);
       }
     },
   },
