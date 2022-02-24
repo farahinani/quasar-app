de9b41201e61b8695bb1db50441fb8cacb74d22a
farahinani <farahinaniahmad@gmail.com>
2022-02-24 16:36:11 +0800

    update test

--- src/pages/test.vue
+++ src/pages/test.vue
@@ -44,6 +44,25 @@ export default defineComponent({
     //   }
     // };
   },
+
+  methods() {
+    window.onload = function () {
+      //create a new instance of shake.js.
+      var myShakeEvent = new Shake({
+        threshold: 15,
+      });
+      // start listening to device motion
+      myShakeEvent.start();
+      // register a shake event
+      window.addEventListener("shake", shakeEventDidOccur, false);
+      //shake event callback
+      function shakeEventDidOccur() {
+        //put your own code here etc.
+        // add();
+        alert("Shake!");
+      }
+    };
+  },
 });
 </script>
 
