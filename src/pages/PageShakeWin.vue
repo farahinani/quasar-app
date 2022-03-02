28ca7d7ad9636ac9b708a2be1821f91172be0c76
farahinani <farahinaniahmad@gmail.com>
2022-03-02 15:26:30 +0800

    update pageshakeandwin 14

--- src/pages/PageShakeWin.vue
+++ src/pages/PageShakeWin.vue
@@ -163,11 +163,11 @@ export default defineComponent({
             Math.abs(oldy - Math.round(event.acceleration.y)) > shakethreshold
           ) {
             if (this.$root.triesCount < this.$root.numTries) {
-              this.$root.triesCount++; //original count is here
+              // this.$root.triesCount++; //original count is here
               alert("shaken !! : try " + this.$root.triesCount);
 
               this.$router.push("/home/shake-and-win/animation");
-
+              this.$root.triesCount++;
               //this.displayTriesNum++; // display until try 4
               // this.$root.triesCount += 1; //try start from 0
             } else {
@@ -185,15 +185,13 @@ export default defineComponent({
     //TEST BUTTON FOR SHAKE
     shakeSuccess() {
       if (this.$root.triesCount < this.$root.numTries) {
-        if (this.$root.triesCount < this.$root.numTries) {
-          // this.$root.triesCount++;
-          alert("shaken !! : try " + this.$root.triesCount);
-          this.$router.push("/home/shake-and-win/animation");
-          this.$root.triesCount++;
-        } else {
-          alert("Last Shake");
-          this.$router.push("/home/shake-and-win/prizes");
-        }
+        // this.$root.triesCount++;
+        alert("shaken !! : try " + this.$root.triesCount);
+        this.$router.push("/home/shake-and-win/animation");
+        this.$root.triesCount++;
+      } else {
+        alert("Last Shake");
+        this.$router.push("/home/shake-and-win/prizes");
       }
     },
   },
