8eba0f07a78eda23f3ffe4726cbee35554e1183f
farahinani <farahinaniahmad@gmail.com>
2022-03-02 12:14:17 +0800

    update pageshakeandwin 7

--- src/pages/PageShakeWin.vue
+++ src/pages/PageShakeWin.vue
@@ -163,12 +163,12 @@ export default defineComponent({
               Math.abs(oldy - Math.round(event.acceleration.y)) > shakethreshold
             ) {
               if (this.$root.triesCount < this.$root.numTries) {
-                this.$root.triesCount++; //original count is here
+                // this.$root.triesCount++; //original count is here
                 alert("shaken !! : try " + this.$root.triesCount);
                 this.$router.push("/home/shake-and-win/animation");
+                this.$root.triesCount++;
                 //this.displayTriesNum++; // display until try 4
                 // this.$root.triesCount += 1; //try start from 0
-                // this.$router.push("/test");
               } else {
                 alert("finish shake");
                 this.$router.push("/home/shake-and-win/prizes");
