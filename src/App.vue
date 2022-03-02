65b0135e752d5fc1331ad5e673dc946ae660ceed
farahinani <farahinaniahmad@gmail.com>
2022-03-02 16:41:08 +0800

    update shake 6

--- src/App.vue
+++ src/App.vue
@@ -10,7 +10,8 @@ export default defineComponent({
   data() {
     return {
       numTries: 0, // customer's trial
-      triesCount: 1,
+      // triesCount: 1,
+      triesCount: 0,
     };
   },
 });
