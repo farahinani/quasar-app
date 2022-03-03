91e81e6f83c586d3985fdb5b59e9537f6a21cfb2
farahinani <farahinaniahmad@gmail.com>
2022-03-03 18:21:52 +0800

    revert back

--- src/pages/PageShakeWin.vue
+++ src/pages/PageShakeWin.vue
@@ -166,12 +166,8 @@ export default defineComponent({
               this.$root.triesCount++;
               alert("shaken !! : try " + this.$root.triesCount);
               this.$router.push("/home/shake-and-win/animation");
-            } else if (this.$root.triesCount == this.$root.numTries) {
-              this.$root.triesCount = 0;
-              alert("finish shake");
-              this.$router.push("/home/shake-and-win/animation");
             } else {
-              alert("No shake left");
+              alert("Finish shake");
               this.$router.push("/home/shake-and-win/prizes");
             }
           }
@@ -210,6 +206,10 @@ export default defineComponent({
       //   this.$router.push("/home/shake-and-win/prizes");
       // }
     },
+  },
+
+  destroyed() {
+    console.log("Destroyed hook: Component is destroyed");
   },
 });
 </script>
--- src/pages/test2.vue
+++ src/pages/test2.vue
@@ -11,13 +11,13 @@
             class="full-width"
             @click="forAndroid()"
           /> -->
-          <p>Enter number of cartons</p>
+          <!-- <p>Enter number of cartons</p>
 
           <form>
             Number of Cartons:
             <input type="text" name="fname" id="packCount" /><br />
             <input type="button" @click="shakeSuccess()" value="Submit" />
-          </form>
+          </form> -->
         </q-card-section>
       </q-card>
     </div>
@@ -31,30 +31,16 @@ export default defineComponent({
   name: "test2",
 
   methods: {
-    // myFunction() {
-    //   let numPacks = document.getElementById("packCount").value;
-
-    //   let numTries = numPacks / 2;
-
-    //   if (numTries < 1) {
-    //     alert("please purchase at least 2 packs");
-    //   } else if (numTries >= 1) {
-    //     let numTriesInt = Math.floor(numTries);
-
-    //     alert("Number of Tries:" + " " + numTriesInt);
+    // shakeSuccess() {
+    //   if (this.$root.triesCount < this.$root.numTries) {
+    //     this.$root.triesCount++;
+    //     if (this.$root.triesCount < this.$root.numTries) {
+    //       alert("shaken !! : try " + this.$root.triesCount);
+    //     } else {
+    //       alert("Last Shake");
+    //     }
     //   }
     // },
-
-    shakeSuccess() {
-      if (this.$root.triesCount < this.$root.numTries) {
-        this.$root.triesCount++;
-        if (this.$root.triesCount < this.$root.numTries) {
-          alert("shaken !! : try " + this.$root.triesCount);
-        } else {
-          alert("Last Shake");
-        }
-      }
-    },
   },
 });
 </script>
