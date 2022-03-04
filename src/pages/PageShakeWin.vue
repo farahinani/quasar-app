bb86b891b80704eb23f2c89dfc362f1809d1d1ee
farahinani <farahinaniahmad@gmail.com>
2022-03-04 13:16:05 +0800

    try unmounted 2

--- src/pages/PageShakeWin.vue
+++ src/pages/PageShakeWin.vue
@@ -130,6 +130,10 @@ export default defineComponent({
     }
   },
 
+  unmounted() {
+    PageShakeWin.unmounted();
+  },
+
   methods: {
     //DETECT PERMISSION FOR DEVICE MOTION WHEN CLICK BUTTON 'START SHAKE'
     requestPermission() {
@@ -178,32 +182,6 @@ export default defineComponent({
         // true
       );
       //}
-    },
-
-    unmounted() {
-      window.removeEventListener("devicemotion", (event) => {
-        if (
-          Math.abs(oldx - Math.round(event.acceleration.x)) > shakethreshold ||
-          Math.abs(oldy - Math.round(event.acceleration.y)) > shakethreshold
-        ) {
-          if (this.$root.triesCount < this.$root.numTries) {
-            this.$root.triesCount++;
-            //this.$root.triesCount += 1;
-            alert("shaken !! : try " + this.$root.triesCount);
-            this.$router.push("/home/shake-and-win/animation");
-          } else if (this.$root.triesCount <= this.$root.numTries) {
-            this.$root.triesCount = 0;
-            this.$root.numTries = 0;
-            alert("Finish shake");
-            this.$router.push("/home/shake-and-win/animation");
-          } else {
-            alert("No shake left");
-            this.$router.replace("/home/shake-and-win/prizes");
-          }
-        }
-        oldx = Math.round(accel.x);
-        oldy = Math.round(accel.y);
-      });
     },
 
     //TEST BUTTON FOR SHAKE
