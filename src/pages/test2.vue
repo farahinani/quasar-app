6e1bff4a4475e5573eb8ffc8f5dd3a55cce767e1
farahinani <farahinaniahmad@gmail.com>
2022-03-04 19:28:42 +0800

    update page test

--- src/pages/test2.vue
+++ src/pages/test2.vue
@@ -2,17 +2,72 @@
   <q-page class="flex flex-center">
     <div class="q-pa-md row items-start q-gutter-md">
       <q-card class="my-card">
-        page test 2
-
         <q-card-section>
+          <div class="text-center">
+            <span class="text-h4"
+              >SHAKE & WIN <br />
+              <span class="text-h5">WITH FLORIDA'S NATURAL</span>
+            </span>
+            <hr />
+            <br />
+          </div>
+          <div class="q-py-lg q-px-md">
+            <div class="text-h5 text-center">
+              You have {{ this.$root.numTries }} tries
+              <q-badge color="teal-10" align="middle">
+                {{ this.$root.triesCount }}/{{ this.$root.numTries }}
+              </q-badge>
+            </div>
+            <br />
+            <div class="text-h5 text-center">
+              <!-- <b>Try {{ trialNumber }}</b> -->
+              <b>Try {{ this.$root.triesCount }}</b>
+            </div>
+            <br />
+            <div class="body-text1 text-center">
+              Shake your device <br />
+              to see what you get!
+            </div>
+          </div>
+
+          <!-- START SHAKE IOS BUTTON -->
+          <div v-if="$q.platform.is.ios" v-on:click="hide = !hide">
+            <p>
+              <q-btn
+                class="full-width"
+                v-if="hide"
+                color="primary"
+                @click.once="requestPermission()"
+                label="Start Shake"
+              >
+              </q-btn>
+            </p>
+          </div>
+
+          <!-- NEXT BUTTON -->
+          <!-- <q-btn
+            to="/home/shake-and-win/prizes"
+            type="submit"
+            fullwidth
+            label="NEXT"
+            color="primary"
+            class="full-width"
+          /> -->
+
+          <!-- SHAKESUCCESS() BUTTON -->
           <q-btn
             type="submit"
             fullwidth
             label="shakeSuccess()"
             color="secondary"
             class="full-width"
-            @click="shakeAlex()"
-        /></q-card-section>
+            @click="shakeSuccess()"
+          />
+
+          test: {{ this.$root.triesCount }} <br />
+          question: {{ this.question }} <br />
+          shake Count: {{ this.shakeCount }}
+        </q-card-section>
       </q-card>
     </div>
   </q-page>
@@ -21,8 +76,7 @@
 <script>
 import { useQuasar } from "quasar";
 import { defineComponent } from "vue";
-import test3 from "src/pages/test3.vue";
-//import { Shake } from "src/pages/shake.js";
+
 var Shake = require("src/pages/shake.js");
 
 export default defineComponent({
@@ -32,31 +86,34 @@ export default defineComponent({
     return {
       alert: false,
       seen: true,
+      hide: true,
     };
   },
 
-  // setup() {
-  //   const $q = useQuasar();
-
-  //   $q.dialog({
-  //     component: test3,
-  //   })
-  //     .onOk(() => {
-  //       console.log("OK");
-  //     })
-  //     .onCancel(() => {
-  //       console.log("Cancel");
-  //     })
-  //     .onDismiss(() => {
-  //       console.log("Called on OK or Cancel");
-  //     });
-  // },
-
   mounted() {
-    this.shakeAlex();
+    const $q = useQuasar();
+    $q.platform.is.desktop;
+    $q.platform.is.android;
+    $q.platform.is.ios;
+
+    if ($q.platform.is.ios) {
+      this.requestPermission();
+    } else if ($q.platform.is.android) {
+      this.shakeAlex();
+    } else {
+      console.log("this is dekstop!");
+    }
   },
 
   methods: {
+    requestPermission() {
+      DeviceMotionEvent.requestPermission().then((response) => {
+        if (response == "granted") {
+          this.shakeAlex();
+        }
+      });
+    },
+
     shakeAlex() {
       window.onload = function () {
         //create a new instance of shake.js.
