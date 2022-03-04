429919573afc746e7192bc6677d1e463672d0ca9
farahinani <farahinaniahmad@gmail.com>
2022-03-05 02:41:22 +0800

    update shakedetector 24

--- src/pages/PageShakeWin.vue
+++ src/pages/PageShakeWin.vue
@@ -119,17 +119,9 @@ export default defineComponent({
       // register a shake event
       window.addEventListener(
         "shake",
-        function () {
+        () => {
           // alert("shaken !! : try ");
-          // if (this.$root.triesCount < this.$root.numTries) {
-          //   this.$root.triesCount++;
-          //   alert("this.$root.triesCount : " + this.$root.triesCount);
-          //   //this.$router.push("/home/shake-and-win/animation");
-          // } else if (this.$root.triesCount == this.$root.numTries) {
-          //   alert("finish shake");
-          //   //this.$router.push("/home/shake-and-win/animation");
-          // }
-          shakeSuccess();
+          this.shakeSuccess();
         },
         false
       );
