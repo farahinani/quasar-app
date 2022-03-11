9cb0092c81914160155a59fd418ba4b3a58ff292
farahinani <farahinaniahmad@gmail.com>
2022-03-11 16:12:54 +0800

    update pop up 2

--- src/pages/PagePrizes.vue
+++ src/pages/PagePrizes.vue
@@ -76,37 +76,42 @@
             <br />
           </q-form>
         </q-card-section>
+
+        <q-dialog v-model="alert">
+          <q-card>
+            <q-card-section>
+              <div class="text-h6">Receipt example</div>
+            </q-card-section>
+
+            <q-card-section class="q-pt-none">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum
+              repellendus sit voluptate voluptas eveniet porro. Rerum blanditiis
+              perferendis totam, ea at omnis vel numquam exercitationem aut,
+              natus minima, porro labore.
+              <br />
+              <img
+                style="
+                  width: 60vw;
+                  display: block;
+                  margin-left: auto;
+                  margin-right: auto;
+                "
+                v-bind:src="require('assets/receipt.png')"
+              />
+            </q-card-section>
+
+            <q-card-actions align="right">
+              <q-btn
+                class="full-width"
+                label="OK"
+                color="primary"
+                v-close-popup
+              />
+            </q-card-actions>
+          </q-card>
+        </q-dialog>
       </q-card>
     </div>
-
-    <q-dialog v-model="alert">
-      <q-card>
-        <q-card-section>
-          <div class="text-h6">Receipt example</div>
-        </q-card-section>
-
-        <q-card-section class="q-pt-none">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum
-          repellendus sit voluptate voluptas eveniet porro. Rerum blanditiis
-          perferendis totam, ea at omnis vel numquam exercitationem aut, natus
-          minima, porro labore.
-          <br />
-          <img
-            style="
-              width: 60vw;
-              display: block;
-              margin-left: auto;
-              margin-right: auto;
-            "
-            v-bind:src="require('assets/receipt.png')"
-          />
-        </q-card-section>
-
-        <q-card-actions align="right">
-          <q-btn class="full-width" label="OK" color="primary" v-close-popup />
-        </q-card-actions>
-      </q-card>
-    </q-dialog>
   </q-page>
 </template>
 
